b0220bddf5c8366ee73046489ec3f3d6749b360d
Léo Delplanque <delplanqueleomail@gmail.cpm>
2021-12-29 01:51:29 +0100

    :sparkles: [App] Different Scene are now working

--- src/App.cpp
+++ src/App.cpp
@@ -36,8 +36,8 @@ App::App(int window_width, int window_height, const FilePath& appPath) :
      _textures(),
      _width(window_width),
      _height(window_height),
-     _camera(_width,_height,_player, _map),
-     _player(Model(((std::string)appPath.dirPath() + "/assets/obj/steve/scene.gltf").c_str()), _map.getSpawnPoint(),0.026f, _map)
+     _player(Model(((std::string)appPath.dirPath() + "/assets/obj/steve/scene.gltf").c_str()), _map.getSpawnPoint(),0.026f, _map),
+     _camera(_width,_height,_player, _map)
 {
     size_callback(window_width, window_height);
 }
@@ -80,105 +80,32 @@ void App::init(){
 }
 
 void App::render(GLFWwindow* window) {
-    glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-    // Clean the back buffer and assign the new color to it
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    //CODE IN GAMESCENE.cpp
 
-    // SKYBOX PART | SETUP AND DRAWING
-    _skybox.setup(_skyboxShader, _camera, _width, _height);
-
-    _steveShader.activate();
-    _camera.Matrix(45.0f, 0.1f, 100.0f);
-
-
-    glm::mat4 model = glm::mat4(1.0f);
-    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    glm::vec3 lightPos = glm::vec3(0.f, 2.f, 7.f);
-    //Light
-    _lightShader.activate();
-    model = glm::mat4(1.0f);
-    model = glm::translate(model, lightPos);
-    model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));    // it's a bit too big for our scene, so scale it down
-    glUniformMatrix4fv(glGetUniformLocation(_lightShader._id, "camMatrix"), 1, GL_FALSE,glm::value_ptr(_camera.getProjMatrix() * _camera.getViewMatrix()));
-    glUniformMatrix4fv(glGetUniformLocation(_lightShader._id, "model"), 1, GL_FALSE, glm::value_ptr(model));
-    _models[4].Draw(_lightShader);
-
-
-    // Tell OpenGL which Shader Program we want to use
-    _steveShader.activate();
-    glUniform4f(glGetUniformLocation(_steveShader._id, "lightColor"), lightColor.x, lightColor.y, lightColor.z,lightColor.w);
-    glUniform3f(glGetUniformLocation(_steveShader._id, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
-    glUniform3f(glGetUniformLocation(_steveShader._id, "camPos"), _camera.getPosition().x, _camera.getPosition().y,_camera.getPosition().z);
-
-    //HUD
-    if (_player.getDistanceToPlayer() != 0) {
-        //INPUTS
-        _player.Inputs(window);
-        _player.render();
-        _hud.DrawHUD(_steveShader, _models[2]);
-    } else { //Game Over
-        _hud.DrawGameOver(_steveShader, _models[3]);
-        if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
-            _player.setDistanceToPlayer(1.f);
-            std::cout << "Restart" << std::endl;
-        }
-    }
-
-    //Player Part
-    _steveShader.activate();
-    glUniformMatrix4fv(glGetUniformLocation(_steveShader._id, "camMatrix"), 1, GL_FALSE,glm::value_ptr(_camera.getProjMatrix() * _camera.getViewMatrix()));
-    _player.Draw(_steveShader);
-
-// Collectibles
-    for (unsigned int i = 0; i < _collectibles.size(); i++) {
-        _collectibles[i].Update(_player);
-        _collectibles[i].Draw(_steveShader, _camera);
-    }
-
-    //Enemies
-    for (unsigned int i = 0; i < _enemies.size(); i++) {
-        _enemies[i].DrawEnemy(_player, _camera, _steveShader);
-    }
-
-
-    // Draw the map
-    //FLOOR PART
-    for (auto &me: _map.getFloor()) {
-        if (me.getModel() != -1) {
-            model = me.getObjectMatrix();
-            model = glm::scale(model,glm::vec3(0.5f, 0.5f, 0.5f));
-            glUniformMatrix4fv(glGetUniformLocation(_steveShader._id, "model"), 1, GL_FALSE, glm::value_ptr(model));
-            _modelsMap[static_cast<unsigned long>(me.getModel())].Draw(_steveShader);
-        }
-    }
-
-    //SECOND FLOOR PART (WALL / JUMP OBSTACLE...)
-    for(auto &me:_map.getSecondFloor()){
-        if(me.getModel() != -1){
-            model = me.getObjectMatrix();
-            model = glm::scale(model,glm::vec3(0.5f, 0.5f, 0.5f));
-            glUniformMatrix4fv(glGetUniformLocation(_steveShader._id, "model"), 1, GL_FALSE, glm::value_ptr(model));
-            _modelsMap[static_cast<unsigned long>(me.getModel())].Draw(_steveShader);
-        }
-    }
-
-    /* VERIF SI ON EST EN TRAIN DE TOURNER
-     *
-     * SI OUI -- ON AUGMENTE X PAR X JUSQU'A MAX 90
-     * SI NON -- ON FAIT RIEN.
-     *
-     * */
-    float valueToRotatePerFrame = 5.f;
-    if(_camera.isTurningLeft() && _camera.getCurrentAngleX() < 90){
-        _camera.rotateLeft(-valueToRotatePerFrame);
-        _camera.setCurrentAngleX(_camera.getCurrentAngleX() + valueToRotatePerFrame);
-    } else if(_camera.isTurningRight() && _camera.getCurrentAngleX() < 90) {
-        _camera.rotateLeft(valueToRotatePerFrame);
-        _camera.setCurrentAngleX(_camera.getCurrentAngleX() + valueToRotatePerFrame);
-    } else {
-        _camera.setCurrentAngleX(0.f);
-        _camera.setTurningLeft(false);
-        _camera.setTurningRight(false);
+    switch (getScene()) {
+        case 0:
+            //mENU OF THE GAME
+            renderMainMenu(window);
+            break;
+        case 1:
+            //GAME ITSELF
+            renderGame(window);
+            break;
+        case 2:
+            //PAUSE MENU
+            renderPauseMenu(window);
+            break;
+        case 3:
+            //END GAME (LOOSE CASE)
+            renderLooseScreen(window);
+            break;
+        case 4:
+            //END GAME (WIN CASE)
+            renderWinScreen(window);
+            break;
+        default:
+            assert("pas possible");
+            break;
     }
 
 }
@@ -186,26 +113,51 @@ void App::render(GLFWwindow* window) {
 void App::key_callback(int key, /*int scancode,*/ int action/*, int mods*/)
 {
 
+    //SCENE SELECTION
+    switch (key) {
+        case 49:
+            //SET TO MAIN MENU SCENE
+            std::cout << "scene menu" << std::endl;
+            setScene(0);
+            break;
+        case 50:
+            //SET GAME SCENE
+            std::cout << "scene jeu" << std::endl;
+            setScene(1);
+            break;
+        case 51:
+            //SET PAUSE MENU SCENE
+            std::cout << "scene pause" << std::endl;
+            setScene(2);
+            break;
+        case 52:
+            //SET LOOSE SCENE
+            std::cout << "scene loose" << std::endl;
+            setScene(3);
+            break;
+        case 53:
+            //SET WIN SCENE
+            std::cout << "scene win" << std::endl;
+            setScene(4);
+            break;
+    }
+
+    std::cout << key << std::endl;
     if(key == 65 && action == GLFW_PRESS){
         _player.turnLeft();
         _camera.turnLeft();
     }
-
     if(key == 68 && action == GLFW_PRESS){
         _player.turnRight();
         _camera.turnRight();
     }
-
-
     if(key == 32 && (action == GLFW_PRESS)){
         _player.startJump();
     }
-
     //L == LOCKING THE CAMERA
     if(key == 76 && (action == GLFW_PRESS)){
         _camera.invertCamLock();
     }
-
     //M == LOCKING THE CAMERA
     if(key == 77 && (action == GLFW_PRESS)){
 
@@ -213,32 +165,106 @@ void App::key_callback(int key, /*int scancode,*/ int action/*, int mods*/)
         _camera.resetAngle();
     }
 
-    //std::cout << "key : " << key << std::endl;
 }
 
 void App::mouse_button_callback(int /*button*/, int /*action*/, int /*mods*/)
 {
+    switch (getScene()) {
+        case 0:
+            //mENU OF THE GAME
+
+            break;
+        case 1:
+            //GAME ITSELF
+
+            break;
+        case 2:
+            //PAUSE MENU
+
+            break;
+        case 3:
+            //END GAME (LOOSE CASE)
+
+            break;
+        case 4:
+            //END GAME (WIN CASE)
+
+            break;
+        default:
+            assert("pas possible");
+            break;
+    }
 }
 
 void App::scroll_callback(double xOffset, double yOffset)
 {
-    if(!_camera.isCamLocked()) {
-        _camera.scrollCallback(xOffset, -yOffset);
+    switch (getScene()) {
+        case 0:
+            //mENU OF THE GAME
+
+            break;
+        case 1:
+            //GAME ITSELF
+            if(!_camera.isCamLocked()) {
+                _camera.scrollCallback(xOffset, -yOffset);
+            }
+            break;
+        case 2:
+            //PAUSE MENU
+
+            break;
+        case 3:
+            //END GAME (LOOSE CASE)
+
+            break;
+        case 4:
+            //END GAME (WIN CASE)
+
+            break;
+        default:
+            assert("pas possible");
+            break;
     }
+
 }
 
 void App::cursor_position_callback(double xPos, double yPos, GLFWwindow* window)
 {
 
-    glfwSetCursorPos(window,(float)_width/2,(float)_height/2);
-    //IT CAN ONLY ROTATE WHEN THE USER WANT
-    if(!_camera.isCamLocked()){
-        float rotX = _camera.getSensitivity() * (float)(yPos - ((float)_height/2))/(float)_height;
-        float rotY = _camera.getSensitivity() * (float)(xPos- ((float)_width/2))/(float)_width;
+    switch (getScene()) {
+        case 0:
+            //mENU OF THE GAME
 
-        _camera.rotateLeft(rotY);
-        _camera.rotateUp(rotX);
+            break;
+        case 1:
+            //GAME ITSELF
+            glfwSetCursorPos(window,(float)_width/2,(float)_height/2);
+            //IT CAN ONLY ROTATE WHEN THE USER WANT
+            if(!_camera.isCamLocked()){
+                float rotX = _camera.getSensitivity() * (float)(yPos - ((float)_height/2))/(float)_height;
+                float rotY = _camera.getSensitivity() * (float)(xPos- ((float)_width/2))/(float)_width;
+
+                _camera.rotateLeft(rotY);
+                _camera.rotateUp(rotX);
+            }
+            break;
+        case 2:
+            //PAUSE MENU
+
+            break;
+        case 3:
+            //END GAME (LOOSE CASE)
+
+            break;
+        case 4:
+            //END GAME (WIN CASE)
+
+            break;
+        default:
+            assert("pas possible");
+            break;
     }
+
 
 }
 
