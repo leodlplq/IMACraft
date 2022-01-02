eb8c6058c78fc9a61cd539f6de00cd2e451aae73
Val <valentin.guiller@gmail.com>
2022-01-02 13:24:38 +0100

    🚨Fix merge

--- src/LibCraft/renderEngine/scenes/WinScene.cpp
+++ src/LibCraft/renderEngine/scenes/WinScene.cpp
@@ -13,20 +13,33 @@ void App::renderWinScreen(GLFWwindow *window, double FPS) {
     _player.inMenu(true);
     displayGame(FPS);
 // ====== SCORE ========
-    float textHeightTitle = _textMinecraft.textHeight("SCORE: "+_hud.getScore(), 2.f);
-    float textWidthTitle = _textMinecraft.textWidth("SCORE: "+_hud.getScore(), 2.f) + 20;
+    float textHeightTitle = _textMinecraft.textHeight("SCORE: " + _hud.getScore(), 2.f);
+    float textWidthTitle = _textMinecraft.textWidth("SCORE: " + _hud.getScore(), 2.f) + 20;
     float yPosTitle = (static_cast<float>(_height) - textHeightTitle) - 50;
-    float xPosTitle = (static_cast<float>(_width) / 2) - (textWidthTitle / 2) ;
-    _textMinecraft.renderText(_textShader, "SCORE: "+_hud.getScore(), xPosTitle, yPosTitle , 2.f, _hud.getFontColor());
+    float xPosTitle = (static_cast<float>(_width) / 2) - (textWidthTitle / 2);
+    _textMinecraft.renderText(_textShader, "SCORE: " + _hud.getScore(), xPosTitle, yPosTitle, 2.f, _hud.getFontColor());
 
     KeyBoardListener(window);
 // AFFICHAGE DU SCORE BOARD
-    if(_player.getScore() >= _sauv.getLowerScore()){
+    if (_player.getScore() >= _sauv.getLowerScore()) {
 
-        _sauv.SetNewScore(_player.getScore(),_pseudo);
+        _sauv.SetNewScore(_player.getScore(), _pseudo);
 
-        float yPosB = (static_cast<float>(_height)/1.5f);
-        for(unsigned int i = 0; i<_sauv.getContent().size();i++) {
+        float yPosB = (static_cast<float>(_height) / 1.5f);
+        for (unsigned int i = 0; i < _sauv.getContent().size(); i++) {
+            float textHeight = _textMinecraft.textHeight(_sauv.getContent()[i], 2.f);
+            if (i != 0) {
+                yPosB -= (_textMinecraft.textHeight(_sauv.getContent()[i - 1], 2.f));
+            }
+            float textWidth = _textMinecraft.textWidth(_sauv.getContent()[i], 2.f) + 20;
+            float yPos = yPosB - (textHeight / 2);
+            yPosB = yPos;
+            float xPos = (static_cast<float>(_width) / 2) - (textWidth / 2) - 50;
+            _textMinecraft.renderText(_textShader, _sauv.getContent()[i], xPos, yPos, 2.f, _hud.getFontColor());
+        }
+    } else {
+        float yPosB = (static_cast<float>(_height) / 1.5f);
+        for (unsigned int i = 0; i < _sauv.getContent().size(); i++) {
             float textHeight = _textMinecraft.textHeight(_sauv.getContent()[i], 2.f);
             if (i != 0) {
                 yPosB -= (_textMinecraft.textHeight(_sauv.getContent()[i - 1], 2.f));
@@ -39,30 +52,9 @@ void App::renderWinScreen(GLFWwindow *window, double FPS) {
         }
     }
 
-    else{
-            float yPosB = (static_cast<float>(_height)/1.5f);
-                for(unsigned int i = 0; i<_sauv.getContent().size();i++) {
-                    float textHeight = _textMinecraft.textHeight(_sauv.getContent()[i], 2.f);
-                    if (i != 0) {
-                        yPosB -= (_textMinecraft.textHeight(_sauv.getContent()[i - 1], 2.f));
-                    }
-                    float textWidth = _textMinecraft.textWidth(_sauv.getContent()[i], 2.f) + 20;
-                    float yPos = yPosB - (textHeight / 2);
-                    yPosB = yPos;
-                    float xPos = (static_cast<float>(_width) / 2) - (textWidth / 2) - 50;
-                    _textMinecraft.renderText(_textShader, _sauv.getContent()[i], xPos, yPos, 2.f, _hud.getFontColor());
-                }
-
-                float textWidth = _textMinecraft.textWidth(_sauv.getContent()[i], 2.f) + 20;
-                float yPos = yPosB - (textHeight / 2);
-                yPosB = yPos;
-                float xPos = (static_cast<float>(_width) / 2) - (textWidth / 2) - 50;
-                _textMinecraft.renderText(_textShader, _sauv.getContent()[i], xPos, yPos, 2.f, _hud.getFontColor());
-            }
-        }
-
 
     //BUTTONS
     _buttons[6].render();
     _buttons[7].render();
 
+}
